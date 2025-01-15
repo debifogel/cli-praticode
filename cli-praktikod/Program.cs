2724529a979d10bf5f28863ea8b4644a402afbdf
debifogel <d0548431932@gmail.com>
2025-01-15 21:58:59 +0200

    first

---
+++ cli-praktikod/Program.cs
@@ -0,0 +1,87 @@
+﻿using cli_praktikod;
+using System;
+using System.CommandLine;
+
+int SortName(string a, string b) { return a.CompareTo(b); }
+//implements this function
+int SortSoog(string a, string b) { 
+    string s = a.Substring(a.IndexOf('.') + 1);
+    string s2 = b.Substring(b.IndexOf('.') + 1);
+    return s.CompareTo(s2);
+}
+
+#region option command bundle
+
+
+var rootComand = new RootCommand();
+rootComand.Name = ("root command for file");
+var bundleOptionOpenFile = new Option<FileInfo>("--output","file name and path");
+bundleOptionOpenFile.AddAlias("-o");
+var bundleOptionLanguge = new Option<string> ("--language", "which kode to copy") { IsRequired = true };
+bundleOptionLanguge.FromAmong(
+            ".cs",
+            ".py",
+            ".html",
+            ".ts",
+            ".tsx",
+            ".js",
+            ".css");
+bundleOptionLanguge.AddAlias("-l");
+var bundleOptionSourseKod = new Option<bool> ("--source",()=>true ," write with source kod");
+bundleOptionSourseKod.AddAlias("-sk");
+var bundleOptionSort = new Option<string> ("--sort",()=>"ab", "how to sort").FromAmong("ab","writer");
+bundleOptionSort.AddAlias("-sr");
+var bundleOptionRemoveEmpty = new Option<bool> ("--remove-empty",()=>true, "remove empty lines");
+bundleOptionRemoveEmpty.AddAlias("-rmempty");
+var bundleOptionWriterName = new Option<string> ("--name", "write the name of writer");
+bundleOptionWriterName.AddAlias("-n");
+#endregion
+#region command bundle
+var bundleComand = new Command("bundle", "bundle code to do a one file");
+bundleComand.AddOption(bundleOptionWriterName);
+bundleComand.AddOption (bundleOptionRemoveEmpty);
+bundleComand.AddOption(bundleOptionSort);
+bundleComand.AddOption(bundleOptionSourseKod);
+bundleComand.AddOption(bundleOptionLanguge);
+bundleComand.AddOption(bundleOptionOpenFile);
+#endregion
+var bundleComandtest = new Command("bundletest", "bundle code to do a one file test");
+bundleComandtest.SetHandler(() =>
+{
+    try
+    {
+        File.Create("C:\\Users\\User\\Documents\\learns\\praktiKod\\cli-praktikod\\test.txt");
+
+        WriteTofile.AllCopy(Directory.GetCurrentDirectory(), "test.txt",
+                     WriteTofile.WriteWithoutEmpty,
+                    SortName,
+                    ".*");
+    }
+    catch (Exception e) { Console.WriteLine("Eror:the path is not valid"); }
+});
+
+string[] currentDirectoryContext=Directory.GetDirectories(Directory.GetCurrentDirectory());
+bundleComand.SetHandler((openFile,language,sourceKod,sort,removeEmpty,name)=>
+{
+    try
+    {
+        File.Create(bundleOptionOpenFile.Name);
+        StreamWriter file = new StreamWriter(bundleOptionOpenFile.Name);
+        if (name != "")
+        { WriteTofile.WriteDetailes(name, file); }
+        if (sourceKod)
+        { WriteTofile.WriteDetailes("//"+Environment.ProcessPath, file); }
+        file.Close();
+        WriteTofile.AllCopy(Directory.GetCurrentDirectory(), bundleOptionOpenFile.Name,
+            removeEmpty ? WriteTofile.WriteWithoutEmpty : WriteTofile.Write,
+            sort == "ab" ? SortName : SortSoog,
+            language == "all" ? ".*" : language);
+    }
+    
+    catch (Exception e) { Console.WriteLine("Eror:the path is not valid"); }
+},bundleOptionOpenFile, bundleOptionLanguge,bundleOptionSourseKod,bundleOptionSort,bundleOptionRemoveEmpty,bundleOptionWriterName);
+rootComand.AddOption(bundleOptionOpenFile);
+
+rootComand.AddCommand(bundleComand);
+rootComand.AddCommand(bundleComandtest);
+rootComand.InvokeAsync(args);
---
+++ cli-praktikod/WriteTofile.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cli_praktikod
+{
+    
+    internal class WriteTofile
+    {
+        public static void WriteDetailes(string name, StreamWriter file)
+        {
+            file.WriteLine(name);
+        }
+
+        public static void Write(string path, StreamWriter file)
+        {
+            string content=File.ReadAllText(path);
+            file.WriteLine(content );
+        }
+        public static void WriteWithoutEmpty(string path, StreamWriter file)
+        {
+            string[] lines = File.ReadAllLines(path);
+            var nonEmptyLines = lines.Where(line => !string.IsNullOrWhiteSpace(line));
+            foreach (var line in nonEmptyLines)
+            {
+                file.WriteLine(line);
+            }
+        }
+
+       public  static List<string> GetFilesByExtension(string directory, string extension)
+        {
+            List<string> fileList = new List<string>();
+
+            try
+            {
+                // Get files in the current directory with the specified extension
+                fileList.AddRange(Directory.GetFiles(directory, "*" + extension));
+
+                // Get all subdirectories
+                foreach (var dir in Directory.GetDirectories(directory))
+                {
+                    // Recursively get files in subdirectories with the specified extension
+                    fileList.AddRange(GetFilesByExtension(dir, extension));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+
+            return FilterFiles(fileList);
+        }
+        private static bool isNot(string item)
+        {
+            //check what to do with the exe files
+            string[] s = { ".bin", "sln", ".pdb", ".exe", ".vcxproj", ".xml", ".json", ".app", ".node", ".config",".txt" };
+
+            foreach (var f in s)
+            {
+                if (item.Contains(f))
+                    return false;
+            }
+            return true;
+        }
+        public static List<string>FilterFiles(List<string> files)=> files.Where(item => isNot(item)).ToList();
+
+       public  static List<string> SortList(List<string> items, Func<string, string, int> comparisonFunc)
+        {
+            List<string> sortedList = new List<string>(items);
+            sortedList.Sort(new Comparison<string>(comparisonFunc));
+            return sortedList;
+        }
+       public static void CopiesFiles(List<string> files, string file,Action<string,StreamWriter>CopyFunc)
+       {
+            StreamWriter fileWriter = new StreamWriter(file,true);
+            foreach (var item in files)
+            {
+                CopyFunc(item, fileWriter);
+            }
+            fileWriter.Close();
+       }
+        public static void AllCopy(string directory, string file
+            , Action<string,StreamWriter>CopyFunc, Func<string, string, int> comparisonFunc, string end = ".*")
+        {
+            List<string>list= GetFilesByExtension(directory,end);
+            list= SortList(list,comparisonFunc);
+
+            CopiesFiles(list, file, CopyFunc);
+        }
+
+    }
+}
+
